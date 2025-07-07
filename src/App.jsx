cf83ec4a30b791efbfa10af18554e0629384f04b
Vijaya Giduthuri <vijayagiduthuri2@gmail.com>
2025-07-07 22:52:05 +0530

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function App() {
   return (
-    <div className="">
-
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <h1 className="text-white text-4xl font-bold">🚀 Tailwind is working!</h1>
     </div>
-  )
+  );
 }
 
 export default App;
