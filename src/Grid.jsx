c1794d14b60ec6a23fd22277855eaafd130f0e38
Vijaya Giduthuri <vijayagiduthuri2@gmail.com>
2025-07-08 23:30:12 +0530

    Changes in grid

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -5,16 +5,17 @@ function Grid() {
     <div>
       {/**Grid container */}
       {/* Grid container (block-level by default) */}
-      <div className="">
-        <div></div>
-        <div></div>
+      <div className="grid grid-cols-3 gap-4 bg-gray-300 p-4">
+        <div className="bg-red-400 p-4">Item 1</div>
+        <div className="bg-blue-400 p-4">Item 2</div>
+        <div className="bg-green-400 p-4">Item 3</div>
         <div></div>
       </div>
       {/* Inline-grid container - behaves like inline-block */}
-      <div>
-        <div></div>
-        <div></div>
-      </div>
+      <p className="inline-grid grid-cols-2 gap-2 bg-yellow-100 p-2 mt-4">
+        <span className="bg-pink-400 p-2">A</span>
+        <span className="bg-purple-400 p-2">B</span>
+      </p>
     </div>
   );
 }
